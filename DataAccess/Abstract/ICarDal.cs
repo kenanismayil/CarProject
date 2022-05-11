53295b19025f76b41e1c07a997b776c8b3845a3a
kenanismayil <kenanismayil434@gmail.com>
2022-05-11 18:46:51 +0300

    Car Project

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -10,11 +10,9 @@ namespace DataAccess.Abstract
     public interface ICarDal
     {
         List<Car> GetAll();                         //Tum araba listesini alir.
+        List<Car> GetAllBrandId(int brandId);       //Arabalari markaya gore listeler.
         void Add(Car car);
         void Update(Car car);
         void Delete(Car car);
-
-        List<Car> GetAllBrandId(int brandId);       //Arabalari markaya gore listeler.
-
     }
 }
